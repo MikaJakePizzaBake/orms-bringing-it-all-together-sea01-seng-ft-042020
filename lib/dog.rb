e87cec973a53d66fecf5a7899cc87a78f6e8a485
Trevor Low <tlow94@uw.edu>
2020-05-01 05:55:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -61,7 +61,7 @@ class Dog
     sql <<-SQL
     SELECT*FROM dogs WHERE name == ? 
     SQL
-    DB[:conn].execute(sql,name).map {|row| self.new_from_db(row)}.first
+    DB[:conn].execute(sql,name).map {|row| self.new_from_db(row)}
   end
 
 
