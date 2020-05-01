b10ac5998bd96c5296b142f67ecc20192fe037ee
Trevor Low <tlow94@uw.edu>
2020-05-01 05:32:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -73,9 +73,9 @@ class Dog
 
   def self.find_by_id(id)
     sql = <<-SQL
-    SELECT*FROM dogs WHERE id = ?
+    SELECT*FROM dogs WHERE id = ? LIMIT 1
     SQL
-    DB[:conn].execute(sql).map {|row| self.new_from_db(row)}
+    DB[:conn].execute(sql).map {|row| self.new_from_db(row)}.first
   end
 
 end
