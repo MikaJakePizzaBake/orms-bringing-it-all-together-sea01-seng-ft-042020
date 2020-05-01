bc767ebee9f3cf0aaf4592e9560e0bafa20b0b5f
Trevor Low <tlow94@uw.edu>
2020-05-01 05:38:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -77,7 +77,16 @@ class Dog
     SQL
     DB[:conn].execute(sql, id).map {|row| self.new_from_db(row)}.first
   end
-  def self.find_or_create_by
+  def self.find_or_create_by(name:, breed:)
+    song=DB[:conn].execute("SELECT*FROM dogs WHERE name = ? AND breed = ?;", name, breed)
+    if !dog.empty
+      dog_data = dog[0]
+      dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
+    else
+      dog.create(name:name, breed:breed)
+    end
+    dog
+    
   end
 
 end
