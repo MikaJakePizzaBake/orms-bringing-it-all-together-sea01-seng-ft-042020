91bbe728574316870a187aeb43738508e54c505b
Trevor Low <tlow94@uw.edu>
2020-05-01 04:37:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,4 +6,10 @@ class Dog
     @name = name
     @breed = breed
   end
+  def self.create_table
+    sql =<<-SQL
+    CREATE TABLE IF NONE EXISTS dogs(id INTEGER PRIMARY KEY name TEXT, breed TEXT)
+    SQL
+    DB[:conn].execute(sql)
+  end
 end
