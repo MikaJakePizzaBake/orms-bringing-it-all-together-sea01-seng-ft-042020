c99904b551fc0dd916fe19826c9821c1d5328218
Trevor Low <tlow94@uw.edu>
2020-05-01 05:31:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -75,7 +75,7 @@ class Dog
     sql = <<-SQL
     SELECT*FROM dogs WHERE id = ?
     SQL
-    DB[:conn].execute(sql, id).map {|row| self.new_from_db(row)}
+    DB[:conn].execute(sql).map {|row| self.new_from_db(row)}
   end
 
 end
