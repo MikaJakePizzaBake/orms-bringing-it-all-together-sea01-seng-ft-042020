d555d0b5ccfc8e3302c289ff1d2273fab98873bb
Trevor Low <tlow94@uw.edu>
2020-05-01 05:23:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -54,9 +54,9 @@ class Dog
 
   def self.new_from_db(row)
          Dog.new
-         id = row[0]
-         name= row[1]
-         breed = row[2]
+         @id = row[0]
+         @name= row[1]
+         @breed = row[2]
          Dog.new
      end
 
